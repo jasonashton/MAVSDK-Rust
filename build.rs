e8e6a0d292dca792a4fe8d3f84984e2443c7ab79
Jason Ashton <jasonashton@hey.com>
2024-05-29 13:25:33 -0400

    update everything

--- build.rs
+++ build.rs
@@ -17,18 +17,17 @@ const PLUGINS: &[&str] = &[
 ];
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proto_files: Vec<String> = PLUGINS.iter()
+    let mut proto_files: Vec<String> = PLUGINS
+        .iter()
         .map(|&plugin| format!("proto/protos/{}/{}.proto", plugin, plugin))
         .collect();
+
+    proto_files.push("proto/protos/mavsdk_options.proto".to_string());
 
     let mut config = prost_build::Config::new();
     prost_reflect_build::Builder::new()
         .descriptor_pool("crate::DESCRIPTOR_POOL")
-        .configure(
-            &mut config,
-            &proto_files,
-            &[PROTO_INCLUDE_PATH],
-        )?;
+        .configure(&mut config, &proto_files, &[PROTO_INCLUDE_PATH])?;
 
     tonic_build::configure()
         .build_server(false)
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 use once_cell::sync::Lazy;
 use prost_reflect::DescriptorPool;
 
-static DESCRIPTOR_POOL: Lazy<DescriptorPool> = Lazy::new(|| DescriptorPool::decode(
-    include_bytes!(concat!(env!("OUT_DIR"), "/file_descriptor_set.bin")).as_ref()
-).unwrap());
+static DESCRIPTOR_POOL: Lazy<DescriptorPool> = Lazy::new(|| {
+    DescriptorPool::decode(
+        include_bytes!(concat!(env!("OUT_DIR"), "/file_descriptor_set.bin")).as_ref(),
+    )
+    .unwrap()
+});
 
 pub mod action {
     include!(concat!(env!("OUT_DIR"), "/mavsdk.rpc.action.rs"));
